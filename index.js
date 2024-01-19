adc5e5eef7e8d66e69b103bef82f045e3e960235
jspark922 <jspark922@naver.com>
2024-01-19 15:30:14 +0900

    feat login update

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 /*
     index.html 에서 사용되는 .js
 */
-function login(username, password) {
+function login(username, password, flag) {
     //login 처리
+    if(username !== null && flag){
+        //username값과 flag 값을 체크
+    }
     return true;
 };
 
